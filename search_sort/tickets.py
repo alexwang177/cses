fdf83b2ea02aa0d81186390c4d58ad5e34499059
Alex Wang <jianuo.wang@duke.edu>
2021-11-12 19:38:25 -0500

    implemented segment tree

--- search_sort/tickets.py
+++ search_sort/tickets.py
@@ -8,27 +8,65 @@ import sys
 3 5 5 7 8
 '''
 
-from bst import BST
+
+class SegmentTree:
+
+    # tree is one-indexed for convenience
+
+    def __init__(self, arr):
+
+        self.n = len(arr)
+        n = self.n
+
+        self.tree = [0 for _ in range(2*n)]
+
+        # insert leaf nodes in tree
+        for i in range(n):
+            self.tree[n+i] = arr[i]
+
+        # create higher level nodes
+        for i in range(n-1, 0, -1):
+            self.tree[i] = self.tree[2*i] + self.tree[2*i+1]
+
+    def update(self, idx, val):
+
+        # update value in tree array
+        n = self.n
+        self.tree[idx+n] = val
+
+        i = idx + n
+
+        while i > 1:
+            self.tree[i//2] = self.tree[i] + self.tree[i+1]
+            i = i//2
+
+    def sum(self, l, r):
+
+        sum = 0
+
+        l += self.n
+        r += self.n
+
+        while l <= r:
+
+            if l % 2 == 1:
+                sum += self.tree[l]
+                l += 1
+            if r % 2 == 0:
+                sum += self.tree[r]
+                r -= 1
+
+            l = l//2
+            r = r//2
+
+        return sum
 
 
-# n, m = [int(x) for x in sys.stdin.readline().split(" ")]
-# ticket = [int(x) for x in sys.stdin.readline().split(" ")]
-# cust = [int(x) for x in sys.stdin.readline().split(" ")]
+        # n, m = [int(x) for x in sys.stdin.readline().split(" ")]
+        # ticket = [int(x) for x in sys.stdin.readline().split(" ")]
+        # cust = [int(x) for x in sys.stdin.readline().split(" ")]
+ticket = [5, 8, 6, 3, 2, 7, 2, 6]
+tree = SegmentTree(ticket)
 
-ticket = [-1, 99, 0, 2, 5, 0, 13, 2, 8]
-
-tree = BST()
-
-for t in ticket:
-    tree.add(t)
-
-# for c in cust:
-#     t = tree.find_ticket(c)
-
-#     if t:
-#         print(t)
-#         tree.remove(t)
-#     else:
-#         print(-1)
-
-tree.print_tree()
+print(tree.sum(1, 6))
+print(tree.tree)
