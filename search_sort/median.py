cc1b5a158f00e8abc92f764c9b239a1b90c8024b
Alex Wang <jianuo.wang@duke.edu>
2021-11-28 23:25:30 -0500

    median close

--- search_sort/median.py
+++ search_sort/median.py
@@ -6,6 +6,15 @@ def ria():
     return [int(x) for x in sys.stdin.readline().split()]
 
 
+def get_median(small):
+    return -small[0][0]
+
+
+def switch(h1, h2):
+    x, i = heapq.heappop(h1)
+    heapq.heappush(h2, (-x, i))
+
+
 n, k = ria()
 arr = ria()
 
@@ -14,13 +23,41 @@ small, large = [], []
 for i in range(k):
     heapq.heappush(small, (-arr[i], i))
 
-for i in range(k//2):
-    x, i = heapq.heappop(small)
-    heapq.heappush(large, (-x, i))
+for _ in range(k//2):
+    switch(small, large)
 
-ans = []
+ans = [get_median(small)]
 
 for i in range(k, n):
-    pass
+
+    x = arr[i]
+    prev = i - k
+
+    if x >= large[0][0]:
+        heapq.heappush(large, (x, i))
+
+        # element to 'remove' is in small heap, so we need to add more to small heap
+        if arr[prev] <= large[0][0]:
+            switch(large, small)
+
+    else:
+        heapq.heappush(small, (-x, i))
+
+        if arr[prev] >= large[0][0]:
+            switch(small, large)
+
+    while small and small[0][1] <= prev:
+        heapq.heappop(small)
+
+    while large and large[0][1] <= prev:
+        heapq.heappop(large)
+
+    # print(" ")
+    # print(i)
+    # print(small)
+    # print(large)
+    # print(" ")
+
+    ans.append(get_median(small))
 
 print(*ans)
