f9ab6d2f986072611cbfd7fc562d1eb4f095281c
Alex Wang <jianuo.wang@duke.edu>
2021-12-14 16:48:59 -0600

    coin com 2

---
+++ dp/coin_com2.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def ria():
+    return [int(x) for x in sys.stdin.readline().split()]
+
+
+n, target = ria()
+coins = ria()
+coins.sort()
+MOD = (10**9)+7
+
+dp = [0]*(target+1)
+dp[0] = 1
+
+for c in coins:
+
+    for i in range(target + 1):
+        if i + c > target:
+            break
+        else:
+            dp[i+c] = (dp[i+c] + dp[i]) % MOD
+
+print(dp[target] % MOD)
