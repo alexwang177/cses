8429277b3c43a3639eb770b80c7295c38d89311b
Alex Wang <jianuo.wang@duke.edu>
2022-01-02 12:23:59 -0600

    optimize

--- graph/short_2.py
+++ graph/short_2.py
@@ -5,21 +5,49 @@ def ria():
     return [int(x) for x in sys.stdin.readline().split()]
 
 
+def ria_opt():
+    return map(int, sys.stdin.readline().split())
+
+
+def get_index(i, j):
+    return i + 500 * j
+
+
 n, m, q = ria()
-adj = {}
+MAX = 1 << 60
+dist = [MAX]*250000
 
 for _ in range(m):
-    a, b, w = ria()
+    a, b, w = ria_opt()
+    a -= 1
+    b -= 1
 
-    if a not in adj:
-        adj[a] = []
-    if b not in adj:
-        adj[b] = []
+    idx1 = get_index(a, b)
+    idx2 = get_index(b, a)
 
-    adj[a].append(b, w)
-    adj[b].append(a, w)
+    dist[idx1] = min(dist[idx1], w)
+    dist[idx2] = min(dist[idx2], w)
 
-dist = [[0 if i == j else None for i in range(n)] for j in range(n)]
+for i in range(n):
+    dist[get_index(i, i)] = 0
 
+for k in range(n):
+    for i in range(n):
+
+        if k == i or dist[get_index(i, k)] == MAX:
+            continue
+
+        for j in range(i):
+            new_dist = dist[get_index(i, k)] + dist[get_index(k, j)]
+
+            if new_dist < dist[get_index(i, j)]:
+                dist[get_index(i, j)] = dist[get_index(j, i)] = new_dist
+
+ans = []
 for _ in range(q):
-    a, b = ria()
+    a, b = ria_opt()
+    a -= 1
+    b -= 1
+    ans.append(-1 if dist[get_index(a, b)] == MAX else dist[get_index(a, b)])
+
+print(*ans)
