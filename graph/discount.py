11f1d58da14741f1f6b11493450f309ca0c0602e
Alex Wang <jianuo.wang@duke.edu>
2022-01-06 16:55:34 -0500

    correct but too slow for 2 cases

--- graph/discount.py
+++ graph/discount.py
@@ -17,30 +17,40 @@ for _ in range(m):
 
     adj[a].append((b, w))
 
-dist = [None for _ in range(n + 1)]
-dist[1] = 0
+dist = [[None for _ in range(2)] for _ in range(n + 1)]
+dist[1][0] = 0
+dist[1][1] = 0
 
-visited = set()
+# (cur_dist, node, used)
 pq = [(0, 1, False)]
+visited = set()
 
 while pq:
+
+    # print(pq)
+
     cur_dist, node, used = heappop(pq)
 
-    if node in visited:
+    if (node, used) in visited:
         continue
 
-    visited.add(node)
+    visited.add((node, used))
 
     if node in adj:
         for nei, w in adj[node]:
 
-            if dist[nei] is None or cur_dist + w < dist[nei]:
-                dist[nei] = cur_dist + w
-                heappush(pq, (dist[nei], nei, used))
-
             if not used:
-                if dist[nei] is None or cur_dist + w // 2 < dist[nei]:
-                    dist[nei] = cur_dist + w // 2
-                    heappush(pq, (dist[nei], nei, True))
+                if dist[nei][True] is None or cur_dist + (w // 2) < dist[nei][True]:
+                    dist[nei][True] = cur_dist + (w // 2)
+                    heappush(pq, (cur_dist + (w // 2), nei, True))
 
-print(dist[n])
+            if dist[nei][used] is None or cur_dist + w < dist[nei][used]:
+                dist[nei][used] = cur_dist + w
+                heappush(pq, (cur_dist + w, nei, used))
+
+# print(" ")
+
+# for i in range(1, n + 1):
+#     print(f"{i} {dist[i]}")
+
+print(dist[n][1])
