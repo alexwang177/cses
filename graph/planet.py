4d22b9b801471fa8baafd871bf8e54c1dca709ee
Alex Wang <jianuo.wang@duke.edu>
2022-01-22 20:43:20 -0500

    pass!

--- graph/planet.py
+++ graph/planet.py
@@ -8,10 +8,10 @@ def ria():
 MAXD = 30
 
 
-def jump(a, d):
+def jump(parent, a, d):
     for i in range(MAXD):
         if d & (1 << i):
-            a = parent[a][i]
+            a = parent[index(a, i)]
 
     return a
 
@@ -19,18 +19,24 @@ def jump(a, d):
 n, q = ria()
 adj = ria()
 
+
+def index(i, j):
+    return i * MAXD + j
+
+
 '''
     parent[i][j] = node i's (2^j)-th parent
 '''
-parent = [[0 for _ in range(MAXD)] for _ in range(n+1)]
+
+parent = [0 for _ in range(n * MAXD + MAXD)]
 
 for i in range(1, n+1):
-    parent[i][0] = adj[i-1]
+    parent[index(i, 0)] = adj[i-1]
 
 for d in range(1, MAXD):
     for i in range(1, n+1):
-        parent[i][d] = parent[parent[i][d-1]][d-1]
+        parent[index(i, d)] = parent[index(parent[index(i, d-1)], d-1)]
 
 for _ in range(q):
     a, d = ria()
-    print(jump(a, d))
+    print(jump(parent, a, d))
